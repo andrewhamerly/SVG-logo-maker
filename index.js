886e27f0e9a3c3269c217ac23b2c1ccfeb49b0d1
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-04-25 16:09:30 -0400

    Created file structure and SVG logo maker code foundation.

---
+++ index.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateShape = require('./lib/shapes.js');
+
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What is the logo text? (3 character maximum)',
+        validate: function (input) {
+            if (input.length < 4) {
+                return true;
+              } else {
+                return '3 character maximum.';
+              }
+        }
+    },
+    {
+        type: 'input',
+        name: 'textcolor',
+        message: 'What is your text color?'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Which shape is for this logo?',
+        choices: ['circle', 'triangle', 'square'],
+        default: 'circle',
+    },
+    {
+        type: 'input',
+        name: 'shapecolor',
+        message: 'What shape color for the logo?'
+    }
+];
+
+// TODO: Create a function to write logo file
+function writeFile(fileName, shape) {
+    fs.writeFile(fileName, generateShape(shape), (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}
+
+console.log('----------SVG Logo Maker----------') // Added for styling during prompts
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        console.log('Generated logo.svg');
+        writeFile('./examples/logo.svg', answers); // Use user feedback for writing new file function
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log('Error with current environment'); // Prompt couldn't be rendered in the current environment
+        } else {
+            console.log(error);
+            console.log('Something went wrong.'); // Something else went wrong
+        }
+    });
+}
+
+// Function call to initialize app
+init();
